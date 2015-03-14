4bc46051e7fa5b13dc521e6ed5fe62b186cc947e
DrSmugleaf <javier.frayluis@gmail.com>
2015-03-14 01:53:43 +0100

    Added all of Crash Bandicoot's stats

--- data/characters/crashbandicoot.lua
+++ data/characters/crashbandicoot.lua
@@ -3,6 +3,20 @@ local crashBandicootBalanced = GetModConfigData("CRASHBANDICOOT_BALANCED")
 local function balanceCrashBandicootStats(inst)
 
 	local crashBandicootStats = {
+									health = 100,
+									hunger = 150,
+									sanity = 100,
+									damage = 1.25,
+									insulation = 0,
+									walkSpeed = 1.25,
+									runSpeed = 1.25,
+									dapperness = 0,
+									nightDrain = 1,
+									monsterDrain = 1,
+									strongStomach = false,
+									hungerRate = 1.25,
+
+
 									levelBase = 50,
 									
 									foodPrefab1 = "wumpa",
@@ -15,14 +29,35 @@ local function balanceCrashBandicootStats(inst)
 									initialHealth = 75,
 									initialHunger = 75,
 									initialSanity = 100,
-									initialWalkNerf = 1,
-									initialRunNerf = 1,
+									initialDamage = 1,
+									initialInsulation = 0,
+									initialWalk = 1.25,
+									initialRun = 1.25,
 									
 									finalHealth = 100,
 									finalHunger = 125,
 									finalSanity = 150,
-									finalRunNerf = 1,
+									finalDamage = 1,
+									finalInsulation = 0,
+									finalRun = 1.50,
+									finalWalk = 1.50,
+
+
+									initialHealthNerf = 100,
+									initialHungerNerf = 100,
+									initialSanityNerf = 125
+									initialDamageNerf = 1,
+									initialInsulationNerf = 0,
+									initialWalkNerf = 1,
+									initialRunNerf = 1,
+
+									finalHealthNerf = 150,
+									finalHungerNerf = 150,
+									finalSanityNerf = 200,
+									finalDamageNerf = 1.50,
+									finalInsulationNerf = 0,
 									finalWalkNerf = 1,
+									finalRunNerf = 1,
 								}
 	
 	ModifyStats(inst, crashBandicootStats)
