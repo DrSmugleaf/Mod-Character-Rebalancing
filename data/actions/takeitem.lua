4152e2c31eb6e4b148af3a987204771d3b5885a4
Kzisor <kzisor@gmail.com>
2015-03-26 09:15:29 -0500

    Committing: Characterspecific crash fix.
    Fixed a crash when taking the lure plants leafy meat.

--- data/actions/takeitem.lua
+++ data/actions/takeitem.lua
@@ -1,6 +1,6 @@
 ACTIONS.TAKEITEM._fn = ACTIONS.TAKEITEM.fn
 ACTIONS.TAKEITEM.fn = function(act)
-	local targ = act.invobject
+	local targ = act.target or act.invobject
 
 	if targ.components.characterspecific and not targ.components.characterspecific:CanPickUp(act.doer) then
         return false, "CHARACTERSPECIFIC"
